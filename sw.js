4643c67293666e4c194103772c77733812735a9e
lookhill <lookhill@gmail.com>
2025-03-23 02:32:22 +0100

    Expity-tracker-tranche-1

---
+++ sw.js
@@ -0,0 +1,68 @@
+const CACHE_NAME = 'expiry-tracker-v1'
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/favicon.ico',
+  '/assets/index.css',
+  '/assets/index.js'
+]
+
+// Install service worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache)
+      })
+  )
+})
+
+// Activate service worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName)
+          }
+        })
+      )
+    })
+  )
+})
+
+// Fetch event handler
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached response if found
+        if (response) {
+          return response
+        }
+
+        // Clone the request because it can only be used once
+        const fetchRequest = event.request.clone()
+
+        // Make network request and cache the response
+        return fetch(fetchRequest).then((response) => {
+          // Check if response is valid
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response
+          }
+
+          // Clone the response because it can only be used once
+          const responseToCache = response.clone()
+
+          caches.open(CACHE_NAME)
+            .then((cache) => {
+              cache.put(event.request, responseToCache)
+            })
+
+          return response
+        })
+      })
+  )
+}) 
